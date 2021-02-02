d652ecd0f6e835426e89e0c4cad5871e90570c51
Alejandro Aspinwall <aaspinwall@gmail.com>
2021-02-02 14:14:08 -0500

    working

---
+++ prisma/migrations/20210202185642_add_subtitles/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "subittle" TEXT;
---
+++ prisma/migrations/20210202190730_revert_subtitle/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `subittle` on the `Post` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Post" DROP COLUMN "subittle";
